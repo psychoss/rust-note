f91cbc760855771c6b35cc946e788d15ecbc2c16
psychoss <626377748@qq.com>
2016-01-04 17:24:28 +0800

    changed

--- src/server.rs
+++ src/server.rs
@@ -6,7 +6,7 @@ use context::Context;
 use request::Req;
 
 
-pub fn new_server<A: ToSocketAddrs>(addr: A,context:Context) {
+pub fn new_server<A: ToSocketAddrs>(addr: A, context: Context) {
 
     let sock_addr = addr.to_socket_addrs()
                         .ok()
@@ -14,10 +14,9 @@ pub fn new_server<A: ToSocketAddrs>(addr: A,context:Context) {
                         .expect("Could not parse socket address.");
 
     let server = Server::http(sock_addr).unwrap();
-    let arc_context=Arc::new(context);
-    
-    for  r in server.incoming_requests() {
-        Req::new(arc_context.clone()).dispatch(r);
+    let arc_context = Arc::new(context);
+    let req = Req::new(arc_context.clone());
+    for r in server.incoming_requests() {
+        req.dispatch(r);
     }
 }
-
